580dc4bf274e4b3cfd0543aead8258ff41fcdfa5
candide85 <liamstiduotechnology@gmail.com>
2024-09-23 04:12:38 +0000

    second com 3

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
+    credentials: true,
     origin: [`http://localhost:5173`,`https://portofolio-frontend-beta.vercel.app`],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
