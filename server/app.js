8a43aee9ebfc972420fb81b68af2bba12a5c889b
candide85 <liamstiduotechnology@gmail.com>
2024-09-23 04:01:43 +0000

    second com 2

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: `http://localhost:5173`
+    origin: `https://portofolio-frontend-beta.vercel.app/`
 }))
 
 app.use('/api', mail_router)
